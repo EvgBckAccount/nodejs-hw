49da6a001d1ae399295e6b33369fe47aa8487e04
EvgBck <grandblacklist@gmail.com>
2026-01-03 16:06:39 +0200

    hw3

--- src/models/note.js
+++ src/models/note.js
@@ -1,10 +1,5 @@
 import { model, Schema } from 'mongoose';
-
-// {
-//    "title": "LOTR",
-//     "content":  "great books",
-//     "tag": "Todo"
-// }
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -21,18 +16,7 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: [...TAGS],
       default: 'Todo',
       required: true,
     },
@@ -43,4 +27,6 @@ const noteSchema = new Schema(
   },
 );
 
-export const Note = model('notes', noteSchema);
+noteSchema.index({ title: 'text', content: 'text' });
+
+export const Note = model('Note', noteSchema);
